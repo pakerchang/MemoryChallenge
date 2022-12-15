33b19f1459a729c0fb6cd289c8c3b04907de0a94
paker_z <99458393+paker-zhang@users.noreply.github.com>
2022-12-15 17:07:17 +0800

    project init

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import eslintPlugin from 'vite-plugin-eslint';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    eslintPlugin({
-      include: ['src/**/*.{js,vue,ts}', 'src/*.{js,vue,ts}'],
-    }),
-  ],
-});
+  plugins: [vue()],
+})
